eb8a41ee78bc7142af8a571b36a0658c3cd408a9
Muhammad Faizan <mohammadfazan624@gmail.com>
2025-12-18 22:36:18 +0500

    Adds example of running multiple threads with Runnable
    Demonstrates basic multithreading by implementing two Runnable classes
    and starting them in separate threads. Helps illustrate thread creation
    and concurrent execution concepts in Java.

---
+++ Thread_and_processors/MultiT.java
@@ -0,0 +1,29 @@
+class Processor1 implements Runnable {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Processor 1 - Count: " + i);
+        }
+    }
+}
+
+class Processor2 implements Runnable {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Processor 2 - Count: " + i);
+        }
+    }
+}
+
+public class MultiT{
+    public static void main(String []args){
+        Processor1 p1 = new Processor1();
+        Processor2 p2 = new Processor2();
+
+
+        // Creating threads from runable class
+        Thread t1 = new Thread(p1);
+        Thread t2 = new Thread(p2);
+        t1.start();
+        t2.start();
+    }
+}
