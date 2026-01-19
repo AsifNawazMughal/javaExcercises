785c929e0f2c80fa758c5ad5ab598f6c328f63b0
Asif Nawaz <161076678+AsifNawazMughal@users.noreply.github.com>
2026-01-19 20:54:26 +0500

    gui

---
+++ GUI/MyFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class MyFrame {
+    public static void main(String[] args){
+        JFrame j1 = new JFrame("My Login Page");
+        j1.setSize(800,800);
+        j1.setVisible(true);
+        j1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
